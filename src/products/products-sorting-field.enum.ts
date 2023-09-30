80ee83490a82fab24ebb9136e9d9a2a61b1d73fa
Rui H. Banki <ruihbanki@gmail.com>
2023-09-30 08:39:32 -0300

    Remove created at sort

--- src/products/products-sorting-field.enum.ts
+++ src/products/products-sorting-field.enum.ts
@@ -3,7 +3,6 @@ import { registerEnumType } from '@nestjs/graphql';
 export enum ProductsSortingField {
   NAME = 'name',
   PRICE = 'price',
-  CREATED_AT = 'createdAt',
 }
 
 registerEnumType(ProductsSortingField, {
